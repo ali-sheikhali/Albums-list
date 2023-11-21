8b0b283c07401918b4a12240e10d1bff929da86c
ali-sheikhali <ali.sheikhali1998@gmail.com>
2023-11-21 14:22:57 +0330

    first commit

---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
+
+const albumsApi = createApi({
+  reducerPath: 'albums',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3005',
+  }),
+  endpoints(builder) {
+    return {
+      addAlbum: builder.mutation({
+        query: (user) => {
+          return {
+            url: '/albums',
+            method: 'POST',
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: '/albums',
+            params: {
+              userId: user.id,
+            },
+            method: 'GET',
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export { albumsApi };
